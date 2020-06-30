ccbf91f5452e1436d678fd4596d75339c317b33b
bonas <valenfbi@gmail.com>
2020-06-30 11:45:01 -0300

    Agregue recycle view rutinas

---
+++ app/src/main/java/com/example/appclientes2/Adapters/AdapterRoutine.kt
@@ -0,0 +1,60 @@
+package com.example.appclientes2.Adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appclientes2.Entities.Routine
+import com.example.appclientes2.R
+
+class RoutinesListAdapter (private var routinesList: MutableList<Routine>,val adapterOnClick : () -> Unit) : RecyclerView.Adapter<RoutinesListAdapter.RoutineHolder>() {
+
+    companion object {
+
+        private val TAG = "RoutineListAdapter"
+    }
+
+    class RoutineHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+        private var view: View
+
+        init {
+            this.view = v
+        }
+
+        fun bindItems(model: Routine) {
+            val txt: TextView = view.findViewById(R.id.txt_name_item)
+            txt.text = model.name
+            // itemView.imageIv.setImageResource(model.image)
+        }
+
+        fun getCardLayout(): CardView {
+            return view.findViewById(R.id.card_package_item)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoutineHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_routine, parent, false)
+        return (RoutineHolder(view))
+    }
+
+    override fun getItemCount(): Int {
+        return routinesList.size
+    }
+
+    fun setData(newData: ArrayList<Routine>) {
+        this.routinesList = newData
+        this.notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: RoutineHolder, position: Int) {
+        holder.bindItems(routinesList[position])
+        holder.getCardLayout().setOnClickListener {
+            adapterOnClick()
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/appclientes2/RoutinesFragments/RoutinesListFragment.kt
+++ app/src/main/java/com/example/appclientes2/RoutinesFragments/RoutinesListFragment.kt
@@ -5,15 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appclientes2.Adapters.RoutinesListAdapter
+import com.example.appclientes2.Entities.Routine
 import com.example.appclientes2.R
+import com.google.android.material.snackbar.Snackbar
 
 class RoutinesListFragment : Fragment() {
 
-       override fun onCreateView(
+    lateinit var v : View
+    lateinit var recRoutines : RecyclerView
+
+    var routine: MutableList<Routine> = ArrayList<Routine>()
+
+    private lateinit var routinesListAdapter: RoutinesListAdapter
+    private lateinit var gridLayoutManager : GridLayoutManager
+
+    companion object {
+        fun newInstance() = RoutinesListFragment()
+    }
+
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_routines_list, container, false)
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_routines_list, container, false)
+        recRoutines= v.findViewById(R.id.rec_routines)
+        return v
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        routine.add(Routine("Rutina 1"))
+        routine.add(Routine("Rutina"))
+        routine.add(Routine("Emilio"))
+        routine.add(Routine("Luis"))
+        routine.add(Routine("Carlos"))
+        routine.add(Routine("David"))
+
+        recRoutines.setHasFixedSize(true)
+        gridLayoutManager = GridLayoutManager(context,2, LinearLayoutManager.VERTICAL,false)
+        recRoutines.layoutManager = gridLayoutManager
+        routinesListAdapter= RoutinesListAdapter(routine!!){onItemClick()}
+        //routinesListAdapter = RoutinesListAdapter(routine)
+        recRoutines.adapter = routinesListAdapter
+
+    }
+
+    public fun onItemClick(){
+        Snackbar.make(v,"click", Snackbar.LENGTH_SHORT).show()
+        val action1to2 = RoutinesListFragmentDirections.actionDestinationRoutinesToSpecificRoutineFragment()
+
+        v.findNavController().navigate(action1to2)
     }
 
 }
