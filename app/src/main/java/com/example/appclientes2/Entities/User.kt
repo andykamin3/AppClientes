058ce42ca84596ed0e7bc3d1fa928714d69ae36d
Joacocarry <joacocarry@gmail.com>
2020-07-03 23:12:47 -0300

    Main fragment hecho

--- app/src/main/java/com/example/appclientes2/Entities/User.kt
+++ app/src/main/java/com/example/appclientes2/Entities/User.kt
@@ -2,7 +2,7 @@ package com.example.appclientes2.Entities
 
 import android.widget.ListAdapter
 
-class User(name : String, birthDate : String, weight: Double, height: Int, objective: String, favorite: ListAdapter,
+class User(name : String, birthDate : String, weight: Double, height: Int, objective: String,// favorite: ListAdapter,
            image: String) {
 
     lateinit var name: String
@@ -10,7 +10,7 @@ class User(name : String, birthDate : String, weight: Double, height: Int, objec
     var weight: Double
     var height: Int
     lateinit var objective: String
-    lateinit var favorite: ListAdapter
+ //   lateinit var favorite: ListAdapter
     lateinit var image: String
 
     init{
@@ -19,7 +19,7 @@ class User(name : String, birthDate : String, weight: Double, height: Int, objec
         this.weight = weight
         this.height = height
         this.objective = objective
-        this.favorite = favorite
+//        this.favorite = favorite
         this.image = image
 
     }
--- app/src/main/java/com/example/appclientes2/ExtraFragments/ProfileFragment.kt
+++ app/src/main/java/com/example/appclientes2/ExtraFragments/ProfileFragment.kt
@@ -5,19 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.appclientes2.Entities.Routine
+import com.example.appclientes2.Entities.User
+import com.example.appclientes2.GlideApp
 import com.example.appclientes2.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ProfileFragment : Fragment() {
 
     lateinit var v : View
+    lateinit var ProfilseUserIv : ImageView
+    lateinit var txtName : TextView
+    lateinit var txtBirthdate : TextView
+    lateinit var txtWeight : TextView
+    lateinit var txtHeight : TextView
+    lateinit var txtObjective : TextView
+    lateinit var btnSignOff : Button
+    lateinit var btnChangeProfileData : Button
+
+    lateinit var user : User
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.fragment_profile, container, false)
+        ProfilseUserIv = v.findViewById(R.id.imageViewUser)
+        txtName = v.findViewById(R.id.textViewNameUser)
+        txtBirthdate = v.findViewById(R.id.textViewBirthdate)
+        txtWeight = v.findViewById(R.id.textViewWeight)
+        txtHeight = v.findViewById(R.id.textViewHeight)
+        txtObjective = v.findViewById(R.id.textViewObjective)
+        btnSignOff = v.findViewById(R.id.buttonSignOff)
+        btnChangeProfileData = v.findViewById(R.id.buttonChangeProfileData)
+
+        user = User("Usuario 1", "28/02/97",100.42,194,"Disminuir grasa corporal","https://firebasestorage.googleapis.com/v0/b/appclientes-b9820.appspot.com/o/User1.jpg?alt=media&token=153b6f2f-d74e-4afb-b7ef-1c0319f3b996")
 
         return v
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        txtName.text =  user.name
+        txtBirthdate.text = "Fecha de nacimiento:  " + user.birthDate
+        txtWeight.text = "Peso:  " + user.weight.toString()
+        txtHeight.text = "Altura:  " + user.height.toString()
+        txtObjective.text = "Objetivo:  " + user.objective
+        GlideApp.with(requireContext())
+            .load(user.image)
+            .into(ProfilseUserIv)
+
+        /*btnChangeProfileData.setOnClickListener{
+
+        }
+
+        btnSignOff.setOnClickListener{
+
+        }*/
+    }
 }
